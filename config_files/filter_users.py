7012250096179dcb9f061af3cd8b5357e8fc5d4c
hassaangatta <hassaangatta@gmail.com>
2024-09-10 17:53:01 +0500

    dd

--- config_files/filter_users.py
+++ config_files/filter_users.py
@@ -4,17 +4,19 @@ import sys
 
 def get_pr_author(repo_name, pr_number):
     token = os.getenv('GITHUB_TOKEN')
+    print(token)
     g = Github(token)
     
     repo = g.get_repo(repo_name)
     pr = repo.get_pull(pr_number)
 
     pr_author = str(pr.user.login.strip())
+    print(pr_author)
     return pr_author
 
 if __name__ == "__main__":
 
     repo_name = 'hassaangatta/jk_test'  
-    pr_number = int(os.getenv('GITHUB_PR_NUMBER'))  
+    pr_number = int(os.getenv('GITHUB_PR_NUMBER'))
     author = get_pr_author(repo_name, pr_number)
     print(author)
